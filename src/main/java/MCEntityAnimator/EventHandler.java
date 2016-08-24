a24a20dc39497676c42d47ca16e1095d6102d607
Joe Early <joseph.early@btinternet.com>
2016-08-24 12:08:24 +0100

    Changed Animator Main menu to extends GuiScreen instead of GuiMainMenu to allow more flexibility.

--- src/main/java/MCEntityAnimator/EventHandler.java
+++ src/main/java/MCEntityAnimator/EventHandler.java
@@ -7,16 +7,13 @@ import MCEntityAnimator.gui.GuiAnimationMainMenu;
 import MCEntityAnimator.gui.GuiBlack;
 import MCEntityAnimator.gui.GuiHandler;
 import MCEntityAnimator.gui.animation.LoginGUI;
-import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.Vec3;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 
