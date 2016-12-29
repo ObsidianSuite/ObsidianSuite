48d4e1871bf6b86b7ddf88eb5b559dde91c44191
Joe Early <joseph.early@btinternet.com>
2016-12-29 13:35:22 +0000

    Removed unused stuff.

--- src/main/java/com/nthrootsoftware/mcea/gui/GuiHandler.java
+++ src/main/java/com/nthrootsoftware/mcea/gui/GuiHandler.java
@@ -1,12 +1,10 @@
 package com.nthrootsoftware.mcea.gui;
 
-import com.nthrootsoftware.mcea.gui.animation.LoginGUI;
 import com.nthrootsoftware.mcea.gui.animation.MainGUI;
 
 public class GuiHandler 
 {
 	
-	public static LoginGUI loginGUI;
 	public static MainGUI mainGui;
 	
 
