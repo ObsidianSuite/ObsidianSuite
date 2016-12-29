803cc07259cb3e2af256279ae60f02da033b383f
Joe Early <joseph.early@btinternet.com>
2016-12-29 13:54:01 +0000

    Begun work on combining files into one and vice versa.

--- src/main/java/testing/Test.java
+++ src/main/java/testing/Test.java
@@ -1,6 +1,10 @@
 package testing;
 
-import com.nthrootsoftware.mcea.distribution.FileHandler;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+
 import com.nthrootsoftware.mcea.gui.frames.AnimationNewFrame;
 
 public class Test 
@@ -8,7 +12,38 @@ public class Test
 	
 	public static void main(String[] args)
 	{
-		new AnimationNewFrame().display();
+		toFiles();
+	}
+	
+	public static void toFiles()
+	{
+		JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new File("C:\\Users\\Joseph\\Desktop\\Minecraft\\MCEntityAnimator\\Animations\\NewSetup\\entities"));
+		fc.showOpenDialog(null);
+		
+		File f = fc.getSelectedFile();
+		
+		File folder = f.getParentFile();
+				
+		String name = f.getName();
+		name = name.substring(0, name.indexOf("."));
+				
+		File modelFile = new File(folder, name + ".obj");
+		File partFile = new File(folder, name + ".pxy");
+		File setupFile = new File(folder, name + ".data");
+		
+		try 
+		{
+			modelFile.createNewFile();
+			partFile.createNewFile();
+			setupFile.createNewFile();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
