394939bebe8342f10cb6dc48a5b10e32afa9c364
DaBigJoe <joseph.early@btinternet.com>
2017-04-14 10:39:33 +0100

    Updated version numbers.

--- Animator/src/main/java/obsidianAnimator/ObsidianAnimator.java
+++ Animator/src/main/java/obsidianAnimator/ObsidianAnimator.java
@@ -20,7 +20,7 @@ public class ObsidianAnimator
 	
     public static final String MODID = "ObsidianAnimator";
     public static final String MODNAME = "Obsidian Animator";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
 	
 	public static final String homePath = Minecraft.getMinecraft().mcDataDir.getAbsolutePath();
 	public static final String animationPath = homePath + "/animation";
