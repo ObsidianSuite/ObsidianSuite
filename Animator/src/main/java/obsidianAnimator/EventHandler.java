0ed433c318406fc55a9e40bd4abef65ff5564495
DaBigJoe <joseph.early@btinternet.com>
2017-05-02 16:09:45 +0100

    Changed main menu back since it wasn't working on export.

--- Animator/src/main/java/obsidianAnimator/EventHandler.java
+++ Animator/src/main/java/obsidianAnimator/EventHandler.java
@@ -1,136 +1,126 @@
 package obsidianAnimator;
 
-import java.awt.Rectangle;
-import java.io.File;
-import java.util.List;
-
-import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.common.ObfuscationReflectionHelper;
-import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.WorldSettings;
-import net.minecraft.world.WorldType;
-import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import obsidianAnimator.data.Persistence;
+import obsidianAnimator.gui.GuiAnimationMainMenu;
 import obsidianAnimator.gui.GuiBlack;
 import obsidianAnimator.gui.frames.HomeFrame;
 
 public class EventHandler 
 {
 
-	boolean updateChecked = false;
-
-	public static final int ANIMATOR_BUTTON_ID = ObsidianAnimator.MODNAME.hashCode();
-
-    @SideOnly(Side.CLIENT)
-    @SubscribeEvent(priority = EventPriority.LOW)
-    public void onInitGuiPost(GuiScreenEvent.InitGuiEvent.Post event)
-    {
-        if(event.gui instanceof GuiMainMenu)
-        {
-            int offsetX = 0;
-            int offsetY = 0; //24?
-            int btnX = event.gui.width / 2 - 124 + offsetX;
-            int btnY = event.gui.height / 4 + 48 + 24 * 2 + offsetY;
-            List buttonList = ObfuscationReflectionHelper.getPrivateValue(GuiScreen.class, event.gui, "buttonList");
-            while(true)
-            {
-                if(btnX < 0)
-                {
-                    if(offsetY <= -48) //give up
-                    {
-                        btnX = 0;
-                        btnY = 0;
-                        break;
-                    }
-                    else
-                    {
-                        offsetX = 0;
-                        offsetY -= 24;
-                        btnX = event.gui.width / 2 - 124 + offsetX;
-                        btnY = event.gui.height / 4 + 48 + 24 * 2 + offsetY;
-                    }
-                }
-
-                Rectangle btn = new Rectangle(btnX, btnY, 20, 20);//Thanks to heldplayer for this.
-                boolean intersects = false;
-                for(int i = 0; i < buttonList.size(); i++)
-                {
-                    GuiButton button = (GuiButton)buttonList.get(i);
-                    if(!intersects)
-                    {
-                        intersects = btn.intersects(new Rectangle(button.xPosition, button.yPosition, button.width, button.height));
-                    }
-                }
-
-                if(!intersects)
-                {
-                    break;
-                }
-
-                btnX += 24; // move to the right to try and find a free space.
-            }
-
-            buttonList.add(new GuiButton(ANIMATOR_BUTTON_ID, btnX, btnY, 20, 20, "A"));
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    @SubscribeEvent
-    public void onButtonPressPre(GuiScreenEvent.ActionPerformedEvent.Pre event)
-    {
-        if(event.gui instanceof GuiMainMenu && event.button.id == ANIMATOR_BUTTON_ID)
-        {
-			String s = "animation_world";
-			File dir = new File(FMLClientHandler.instance().getSavesDir(), s);
-
-			if(!dir.exists())
-			{
-				System.out.println("No animation world found, creating a new one.");
-				
-				WorldType.worldTypes[1].onGUICreateWorldPress();
-
-				WorldSettings.GameType gametype = WorldSettings.GameType.getByName("creative");
-				WorldSettings worldsettings = new WorldSettings(0, gametype, false, false, WorldType.worldTypes[1]);
-				worldsettings.enableCommands();
-
-				Minecraft.getMinecraft().launchIntegratedServer(s, s, worldsettings);
-			}
-			else
-			{
-				System.out.println("Animation world found, loading.");
-				FMLClientHandler.instance().tryLoadExistingWorld(null, s, s);
-			}
-			event.setCanceled(true);
-        }
-    }
+//	public static final int ANIMATOR_BUTTON_ID = ObsidianAnimator.MODNAME.hashCode();
+//
+//	@SideOnly(Side.CLIENT)
+//	@SubscribeEvent(priority = EventPriority.LOW)
+//	public void onInitGuiPost(GuiScreenEvent.InitGuiEvent.Post event)
+//	{
+//		if(event.gui instanceof GuiMainMenu)
+//		{
+//			int offsetX = 0;
+//			int offsetY = 0; //24?
+//					int btnX = event.gui.width / 2 - 124 + offsetX;
+//					int btnY = event.gui.height / 4 + 48 + 24 * 2 + offsetY;
+//					List buttonList = ObfuscationReflectionHelper.getPrivateValue(GuiScreen.class, event.gui, "buttonList");
+//					while(true)
+//					{
+//						if(btnX < 0)
+//						{
+//							if(offsetY <= -48) //give up
+//							{
+//								btnX = 0;
+//								btnY = 0;
+//								break;
+//							}
+//							else
+//							{
+//								offsetX = 0;
+//								offsetY -= 24;
+//								btnX = event.gui.width / 2 - 124 + offsetX;
+//								btnY = event.gui.height / 4 + 48 + 24 * 2 + offsetY;
+//							}
+//						}
+//
+//						Rectangle btn = new Rectangle(btnX, btnY, 20, 20);//Thanks to heldplayer for this.
+//						boolean intersects = false;
+//						for(int i = 0; i < buttonList.size(); i++)
+//						{
+//							GuiButton button = (GuiButton)buttonList.get(i);
+//							if(!intersects)
+//							{
+//								intersects = btn.intersects(new Rectangle(button.xPosition, button.yPosition, button.width, button.height));
+//							}
+//						}
+//
+//						if(!intersects)
+//						{
+//							break;
+//						}
+//
+//						btnX += 24; // move to the right to try and find a free space.
+//					}
+//
+//					buttonList.add(new GuiButton(ANIMATOR_BUTTON_ID, btnX, btnY, 20, 20, "A"));
+//		}
+//	}
+//
+//	@SideOnly(Side.CLIENT)
+//	@SubscribeEvent
+//	public void onButtonPressPre(GuiScreenEvent.ActionPerformedEvent.Pre event)
+//	{
+//		if(event.gui instanceof GuiMainMenu && event.button.id == ANIMATOR_BUTTON_ID)
+//		{
+//			String s = "animation_world";
+//			File dir = new File(FMLClientHandler.instance().getSavesDir(), s);
+//
+//			if(!dir.exists())
+//			{
+//				System.out.println("No animation world found, creating a new one.");
+//
+//				WorldType.worldTypes[1].onGUICreateWorldPress();
+//
+//				WorldSettings.GameType gametype = WorldSettings.GameType.getByName("creative");
+//				WorldSettings worldsettings = new WorldSettings(0, gametype, false, false, WorldType.worldTypes[1]);
+//				worldsettings.enableCommands();
+//
+//				Minecraft.getMinecraft().launchIntegratedServer(s, s, worldsettings);
+//			}
+//			else
+//			{
+//				System.out.println("Animation world found, loading.");
+//				FMLClientHandler.instance().tryLoadExistingWorld(null, s, s);
+//			}
+//			event.setCanceled(true);
+//		}
+//	}
 
 	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public void onClientTick(TickEvent.ClientTickEvent event)
 	{    	    	
+		if(Minecraft.getMinecraft().currentScreen instanceof GuiMainMenu && !(Minecraft.getMinecraft().currentScreen instanceof GuiAnimationMainMenu))
+			Minecraft.getMinecraft().displayGuiScreen(new GuiAnimationMainMenu());
+
 		if(Minecraft.getMinecraft().inGameHasFocus && Minecraft.getMinecraft().currentScreen == null && MinecraftServer.getServer().getWorldName().equals("animation_world"))
 		{
 			Minecraft.getMinecraft().displayGuiScreen(new GuiBlack());
 			new HomeFrame().display();
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void onWorldSave(WorldEvent.Save event)
 	{
 		Persistence.save();
 	}
-	
+
 	@SubscribeEvent
 	public void onWorldSave(WorldEvent.Load event)
 	{
---
+++ Animator/src/main/java/obsidianAnimator/gui/GuiAnimationMainMenu.java
@@ -0,0 +1,86 @@
+package obsidianAnimator.gui;
+
+import java.io.File;
+
+import org.lwjgl.opengl.GL11;
+
+import cpw.mods.fml.client.FMLClientHandler;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiButtonLanguage;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.WorldSettings;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.storage.ISaveFormat;
+import net.minecraft.world.storage.WorldInfo;
+
+public class GuiAnimationMainMenu extends GuiMainMenu
+{
+	
+    
+	/**
+     * Adds the buttons (and other controls) to the screen in question.
+     */
+    public void initGui()
+    {
+    	super.initGui();
+    	
+        int startY = this.height / 4 + 50;
+        int spaceY = 22;
+        
+        this.buttonList.clear();
+        addSingleplayerMultiplayerButtons(startY, spaceY);
+		this.buttonList.add(new GuiButton(10, this.width / 2 - 100, startY + spaceY * 2, "Animator"));
+        this.buttonList.add(new GuiButton(0, this.width / 2 - 100, startY + spaceY * 4, 98, 20, I18n.format("menu.options", new Object[0])));
+        this.buttonList.add(new GuiButton(4, this.width / 2 + 2, startY + spaceY * 4, 98, 20, I18n.format("menu.quit", new Object[0])));
+        this.buttonList.add(new GuiButtonLanguage(5, this.width / 2 - 124, startY + spaceY * 4));
+    }
+    
+    /**
+     * Adds Singleplayer and Multiplayer buttons on Main Menu for players who have bought the game.
+     */
+    private void addSingleplayerMultiplayerButtons(int y, int spaceY)
+    {
+        this.buttonList.add(new GuiButton(1, this.width / 2 - 100, y, I18n.format("menu.singleplayer", new Object[0])));
+        this.buttonList.add(new GuiButton(2, this.width / 2 - 100, y + spaceY * 1, I18n.format("menu.multiplayer", new Object[0])));
+        GuiButton realmsButton = new GuiButton(14, this.width / 2 - 100, y + spaceY * 3, I18n.format("menu.online", new Object[0]));
+        GuiButton fmlModButton = new GuiButton(6, this.width / 2 - 100, y + spaceY * 3, "Mods");
+        fmlModButton.xPosition = this.width / 2 + 2;
+        realmsButton.width = 98;
+        fmlModButton.width = 98;
+        this.buttonList.add(realmsButton);
+        this.buttonList.add(fmlModButton);
+    }
+	
+    @Override
+    protected void actionPerformed(GuiButton button)
+    {
+    	super.actionPerformed(button);
+
+        if(button.id == 10)
+		{
+			String s = "animation_world";
+			File dir = new File(FMLClientHandler.instance().getSavesDir(), s);
+
+			if(!dir.exists())
+			{
+				System.out.println("No animation world found, creating a new one.");
+				
+				WorldType.worldTypes[1].onGUICreateWorldPress();
+
+				WorldSettings.GameType gametype = WorldSettings.GameType.getByName("creative");
+				WorldSettings worldsettings = new WorldSettings(0, gametype, false, false, WorldType.worldTypes[1]);
+				worldsettings.enableCommands();
+
+				this.mc.launchIntegratedServer(s, s, worldsettings);
+			}
+			else
+			{
+				System.out.println("Animation world found, loading.");
+				FMLClientHandler.instance().tryLoadExistingWorld(null, s, s);
+			}
+		}
+    }
+
+}
