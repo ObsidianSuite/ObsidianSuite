3fa45db716e32985e36dd958c93d254278819fe0
DaBigJoe <joseph.early@btinternet.com>
2017-06-15 18:38:43 +0100

    Updated for new repo.

--- Animator/src/main/java/obsidianAnimator/ObsidianAnimator.java
+++ Animator/src/main/java/obsidianAnimator/ObsidianAnimator.java
@@ -21,8 +21,8 @@ public class ObsidianAnimator
 	
     public static final String MODID = "ObsidianAnimator";
     public static final String MODNAME = "Obsidian Animator";
-    public static final String VERSION = "0.3.3-Alpha";
-    private static final String VERSION_LINK = "https://raw.githubusercontent.com/DaBigJoe/ObsidianSuite/master/VERSION.json";
+    public static final String VERSION = "0.3.4-Alpha";
+    private static final String VERSION_LINK = "https://raw.githubusercontent.com/ObsidianSuite/ObsidianSuite/master/VERSION.json";
     
 	@Mod.Instance("ObsidianAnimator")
 	public static ObsidianAnimator instance;
