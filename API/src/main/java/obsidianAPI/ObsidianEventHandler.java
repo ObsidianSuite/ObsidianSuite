78fc2049a565131606402f758b871ed8d909bcd3
DaBigJoe <dabigjoe97@gmail.com>
2017-09-15 14:36:23 +0100

    Fixed registry.

--- API/src/main/java/obsidianAPI/ObsidianEventHandler.java
+++ API/src/main/java/obsidianAPI/ObsidianEventHandler.java
@@ -3,7 +3,6 @@ package obsidianAPI;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -12,23 +11,11 @@ import net.minecraftforge.fml.relauncher.Side;
 import obsidianAPI.network.AnimationNetworkHandler;
 import obsidianAPI.network.MessagePlayerLimbSwing;
 import obsidianAPI.properties.EntityAnimationProperties;
-import obsidianAPI.properties.EntityAnimationPropertiesClient;
 import obsidianAPI.properties.EntityAnimationPropertiesProvider;
 import obsidianAPI.registry.AnimationRegistry;
 
 public class ObsidianEventHandler 
 {
-
-	@SubscribeEvent
-	public void onEntityConstructing(EntityConstructing e)
-	{
-		Entity entity = e.getEntity();
-		if(AnimationRegistry.isRegisteredClass(entity.getClass())) {
-			EntityAnimationPropertiesProvider.register(entity, Side.SERVER);
-			if(FMLCommonHandler.instance().getEffectiveSide().isClient())
-				EntityAnimationPropertiesProvider.register(entity, Side.CLIENT);
-		}
-	}
 
 	@SubscribeEvent
 	public void onEntityUpdate(LivingUpdateEvent e) {
@@ -47,8 +34,15 @@ public class ObsidianEventHandler
 
 	@SubscribeEvent
 	public void onEntityJoin(EntityJoinWorldEvent e) {
-		if(e.getEntity().world.isRemote)
+		Entity entity = e.getEntity();
+		if(entity.world.isRemote)
 			ObsidianEventHandlerClient.handleOnEntityJoin(e);
+
+		if(AnimationRegistry.isRegisteredClass(entity.getClass())) {
+			EntityAnimationPropertiesProvider.register(entity, Side.SERVER);
+			if(FMLCommonHandler.instance().getEffectiveSide().isClient())
+				EntityAnimationPropertiesProvider.register(entity, Side.CLIENT);
+		}
 	}
 
 }
