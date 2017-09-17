6cd31923d7c07d118b266a64d4d70fa509ad0d07
DaBigJoe <dabigjoe97@gmail.com>
2017-09-17 21:41:48 +0100

    Updated gradle version numbers.

--- Mod/src/main/java/obsidianAnimations/ObsidianAnimations.java
+++ Mod/src/main/java/obsidianAnimations/ObsidianAnimations.java
@@ -17,7 +17,7 @@ public class ObsidianAnimations
 
 	public static final String MODID = "obsidian_animations";
 	public static final String MODNAME = "Obsidian Animations";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "1.0.0";
 
 	@Mod.Instance("ObsidianAnimations")
 	public static ObsidianAnimations instance;
