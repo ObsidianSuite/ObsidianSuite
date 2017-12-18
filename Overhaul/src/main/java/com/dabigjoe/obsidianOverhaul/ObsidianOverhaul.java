5ea8cc4b5408b0638aea54aac3732abe65e99e11
DaBigJoe <dabigjoe97@gmail.com>
2017-12-18 18:24:46 +0000

    Updated version.

--- Overhaul/src/main/java/com/dabigjoe/obsidianOverhaul/ObsidianOverhaul.java
+++ Overhaul/src/main/java/com/dabigjoe/obsidianOverhaul/ObsidianOverhaul.java
@@ -18,7 +18,7 @@ public class ObsidianOverhaul
 
 	public static final String MODID = "obsidian_overhaul";
 	public static final String MODNAME = "Obsidian Overhaul";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "0.0.1";
 
 	@Mod.Instance
 	public static ObsidianOverhaul instance;
